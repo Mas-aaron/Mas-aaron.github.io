71abd0429b400fa76196aa40ae062a936300c043
Mas-aaron <153190021+Mas-aaron@users.noreply.github.com>
2025-07-28 22:29:06 +0300

    Fix: Correct render.yaml and clean up settings

--- backend/food_delivery/settings.py
+++ backend/food_delivery/settings.py
@@ -98,10 +98,8 @@ ASGI_APPLICATION = 'food_delivery.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 if not DEBUG:
-    db_from_env = os.environ.get('DATABASE_URL')
-    print(f"--- Attempting to connect to database with URL: {db_from_env} ---")
     DATABASES = {
-        'default': dj_database_url.parse(db_from_env)
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
     DATABASES['default']['CONN_MAX_AGE'] = 600
 else:
