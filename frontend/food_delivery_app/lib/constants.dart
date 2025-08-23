70c67ef893c29e76d1f64801f2ca858e1ad827fe
Mas-aaron <153190021+Mas-aaron@users.noreply.github.com>
2025-08-23 12:18:41 +0300

    config changes

--- frontend/food_delivery_app/lib/constants.dart
+++ frontend/food_delivery_app/lib/constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 const String googleMapsApiKey = 'AIzaSyB2_rwYdL9Tr_LWV7PDfn83n8-2_6KVahs'; // Replace with your key
 
 // --- LOCAL DEVELOPMENT --- 
-// Use '10.4.45.57' for physical device, 10.0.2.2 for Android Emulator
+// Use '10.4.45.171' for physical device, 10.0.2.2 for Android Emulator
 // IMPORTANT: Replace with your computer's local IP address for physical device/emulator testing.
 // Find it by running 'ipconfig' (Windows) or 'ifconfig' (macOS/Linux) in your terminal.
-const String _localIpAddress = kIsWeb ? '127.0.0.1' : '10.5.55.18';
+const String _localIpAddress = kIsWeb ? '127.0.0.1' : '10.4.45.171';
 
-const String baseUrl = 'http://10.5.55.18:8000/api';
+const String baseUrl = 'http://10.4.45.171:8000/api';
 const String baseWebsocketUrl = 'ws://$_localIpAddress:8000';
 const String websocketUrl = '$baseWebsocketUrl/ws/notifications/';
--- frontend/restaurant_dashboard_new/lib/constants.dart
+++ frontend/restaurant_dashboard_new/lib/constants.dart
@@ -1,3 +1,3 @@
 // TODO: Replace with your local network IP address
-const String baseUrl = 'http://10.5.55.18:8000/api';
-const String webSocketUrl = 'ws://10.5.55.18:8000';
+const String baseUrl = 'http://10.4.45.171:8000/api';
+const String webSocketUrl = 'ws://10.4.45.171:8000';
